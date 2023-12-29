3bc6c514750e2d82d49e14d32df49048e9dea923
ItayElf <thh123321@gmail.com>
2023-12-29 13:15:18 +0200

    Feat: added user fetch hook

---
+++ src/interfaces/userData.ts
@@ -0,0 +1,5 @@
+export default interface UserData {
+    name: String
+    token: String
+    connected_to: String | null
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,18 @@
 import LoadingCirle from "../components/loadingCircle";
+import useFetchUser from "../hooks/useFetchUser";
 
 function Home() {
-  return (
-    <div className="h-[100vh] grid bg-center">
-      <LoadingCirle />
-    </div>
-  );
+  const userData = useFetchUser();
+
+  if (userData == null) {
+    return (
+      <div className="h-[100vh] grid bg-center">
+        <LoadingCirle />
+      </div>
+    );
+  }
+
+  return <></>;
 }
 
 export default Home;
