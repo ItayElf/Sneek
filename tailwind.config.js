d55cda1906c0d7642fb4f40f1d3514db7a99cc03
ItayElf <thh123321@gmail.com>
2023-12-29 12:25:30 +0200

    Feat: added new typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        chakra: ["Chakra Petch", "sans-serif"],
+        glitch: ["Rubik Glitch", "display"],
+      },
+    },
   },
   plugins: [],
 };
