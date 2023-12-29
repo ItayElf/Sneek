44d85d80a2f7edff407597a8169c765214089c0e
ItayElf <thh123321@gmail.com>
2023-12-29 12:59:41 +0200

    Feat: added loading circle

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    "path": "/",
+    element: <Home />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import LoadingCirle from "../components/loadingCircle";
+
+function Home() {
+  return (
+    <div className="h-[100vh] grid bg-center">
+      <LoadingCirle />
+    </div>
+  );
+}
+
+export default Home;
